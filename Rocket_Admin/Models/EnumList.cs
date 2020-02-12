5d5879e18a9eaa51c6acdc2e17a1aa526e7ccbbc
JingRui0607 <phrost0428123@gmail.com>
2020-02-12 17:26:14 +0800

    Create EnumList Model

---
+++ Rocket_Admin/Models/EnumList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rocket_Admin.Models
+{
+    public class EnumList
+    {
+
+        public enum EnableType
+        {
+            開 = 1,
+            關 = 0
+        }
+        public enum BoolType
+        {
+            是 = 1,
+            否 = 0
+        }
+        public enum GenderType
+        {
+            女,
+            男
+        }
+
+        public enum ClassType
+        {
+            陪訓中 = 1,
+            結訓 = 0,
+            退訓=2
+        }
+
+
+
+    }
+}
